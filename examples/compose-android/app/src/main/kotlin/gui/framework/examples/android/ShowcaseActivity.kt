13508c70356081183242a0019cf5a3074d35fa09
HyperCriSiS <dev.github@w3b.world>
2026-09-09 23:24:01 +0200

    Restore Android Showcase wrapper with safe-area handling

--- examples/compose-android/app/src/main/kotlin/gui/framework/examples/android/ShowcaseActivity.kt
+++ examples/compose-android/app/src/main/kotlin/gui/framework/examples/android/ShowcaseActivity.kt
@@ -1,1 +1,30 @@
-__FROM_FILE__
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package gui.framework.examples.android
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.ui.Modifier
+import gui.framework.examples.showcase.ShowcaseExplorer
+
+class ShowcaseActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .safeDrawingPadding(),
+            ) {
+                ShowcaseExplorer(
+                    platformLabel = "Android target",
+                    stressControlCount = 30,
+                )
+            }
+        }
+    }
+}
